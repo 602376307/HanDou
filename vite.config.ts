bdf780b3bfbf4337994b0d908d0934225b8a5069
Liurun <602376307@qq.com>
2026-06-16 16:57:30 +0800

    部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS === 'true' ? '/Handle-main/' : '/',
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/HanDou/' : '/',
   resolve: {
     alias: {
       '~/': `${path.resolve(__dirname, 'src')}/`,
